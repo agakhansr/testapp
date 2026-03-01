17498a7bd8152ef4e605578245ce0a3e89e4a2aa
agakhansr <agaxanist@gmail.com>
2026-03-01 04:08:35 +0400

    Initial Commit

---
+++ testapp/testappApp.swift
@@ -0,0 +1,17 @@
+//
+//  testappApp.swift
+//  testapp
+//
+//  Created by Agakhan Sarkhanov on 01.03.26.
+//
+
+import SwiftUI
+
+@main
+struct testappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
